2a439a5ed2cc2ea5a0a3f1732c0cdf96689b9b30
Plamen Ivanov <pivanov@foobar.ninja>
2017-11-30 12:20:01 -0500

    Improve Boost build script

--- boost-custom-build.sh
+++ boost-custom-build.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-INSTALL_DIR="/home/pivanov/workspace/cuttlefish/libs"
+INSTALL_DIR="$1/cuttlefish/libs"
+echo "install dir is $INSTALL_DIR"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    echo "install dir not found or not accessible"
+    exit 1
+fi
+
 WITH_LIBS="--with-system --with-thread --with-date_time --with-regex --with-log --with-chrono --with-filesystem"
 
-./b2 --prefix=$INSTALL_DIR dll-path=$INSTALL_DIR/lib $WITH_LIBS toolset=clang variant=release link=shared runtime-link=shared threading=multi $1
+echo "will execute: "
+echo "./b2 --prefix=$INSTALL_DIR dll-path=$INSTALL_DIR/lib $WITH_LIBS toolset=clang variant=release link=shared runtime-link=shared threading=multi $2"
+
+while true; do
+    read -p "do you want to compile and install Boost? " yn
+    case $yn in
+        [Yy]* )
+            ./b2 --prefix=$INSTALL_DIR dll-path=$INSTALL_DIR/lib $WITH_LIBS toolset=clang variant=release link=shared runtime-link=shared threading=multi $2
+            break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
